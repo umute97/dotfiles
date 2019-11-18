52350475c93a238b6c083047ae3407adceccec22
Umut Elicabuk <amiculus.umut@hotmail.de>
2019-11-19 00:38:25 +0100

    add icons to notification

--- dot_config/scripts/executable_audio.sh
+++ dot_config/scripts/executable_audio.sh
@@ -2,6 +2,9 @@
 
 msgId="424242"
 mute=$(pactl list | grep Mute: | head -n1 | cut -b7-)
+volume=$(pactl list | grep Volume | head -n1 | awk {'print $5'} | cut -b-2)
+iconpath="/usr/share/icons/Archdroid-BlueGrey/status/scalable/audio-volume-high-panel.svg"
+iconnonepath="/usr/share/icons/Archdroid-BlueGrey/status/scalable/audio-output-none-panel.svg"
 
 function setvolume () {
 
@@ -16,10 +19,10 @@ function setvolume () {
 
 if [[ $volume == 0 || "$mute" == "yes" ]]; then
      # Show the sound muted notification
-     dunstify -a "changeVolume" -u low -i audio-volume-muted -r "$msgId" "Volume muted" 
- else
+     dunstify -a "changeVolume" -u low -i ${iconnonepath} -r "$msgId" "Volume muted" 
+else
      # Show the volume notification
      setvolume $1 $2
      volume=$(pactl list | grep Volume | head -n1 | awk {'print $5'} | cut -b-2)
-     dunstify -a "changeVolume" -u low -i audio-volume-high -r "$msgId" "Volume: ${volume}%" 
- fi
+     dunstify -a "changeVolume" -u low -i ${iconpath} -r "$msgId" "Volume: ${volume}%" 
+fi
