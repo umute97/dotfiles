9d8f71b9c6c27e9d2363dcf7ca0431d68dd6e296
Umut Elicabuk <ureay@student.kit.edu>
2020-03-08 21:34:55 +0100

    new rice, did a bunch of stuff

---
+++ dot_config/polybar/executable_launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar main &
+polybar side &
+
+echo "Polybar launched..."
